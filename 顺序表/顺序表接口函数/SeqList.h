17d1198ce19d590466403ab7aea7bd773d46188e
leiyaling <3120275158@qq.com>
2022-08-10 23:26:30 +0800

    顺序表的实现

---
+++ 顺序表/顺序表接口函数/SeqList.h
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<assert.h>
+#include<stdlib.h>
+
+typedef int SeqDatatype;
+typedef struct SeqList {
+	SeqDatatype* a;
+	int size;
+	int capacity;
+}SL;
+void SeqListInit(SL* ps);
+void SeqListPrint(SL*ps);
+void SeqListCheckCapacity(SL* ps);
+void SeqListPushBack(SL* ps, SeqDatatype* x);
+void SeqListPopBack(SL* ps);
+void SeqListPushFront(SL* ps, SeqDatatype* x);
+void SeqListPopFront(SL* ps);
+void SeqListFind(SL* ps, SeqDatatype* x);
+void SeqListInsert(SL* ps, int pos, SeqDatatype* x);
+void SeqListErase(SL* ps, int pos);
+void SeqListDestroy(SL* ps);
