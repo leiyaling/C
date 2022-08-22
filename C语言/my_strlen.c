c8da4b494322866993517fedc57a74b1bddd2218
leiyaling <3120275158@qq.com>
2022-08-22 21:52:15 +0800

    递归与非递归求字符串长度

---
+++ C语言/my_strlen.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int my_strlen(char *str)
+{
+	/*int count = 0;
+	while(*str!='\0')
+	{
+		count++;
+		str++;
+	}
+	return count;*/
+	if (*str != '\0')
+	{
+		return 1 + my_strlen(str+1);
+	}
+	else
+	{
+		return 0;
+	}
+}
+int main()
+{
+	char arr[] ="abcd";
+	int len=my_strlen(arr);
+	printf("%d\n", len);
+	return 0;
+}
