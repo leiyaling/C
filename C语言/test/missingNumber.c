8d7a1702213e48b078d4d5d3cbdf22e99ea10f4c
leiyaling <3120275158@qq.com>
2022-10-25 22:51:06 +0800

    消失的数字

---
+++ C语言/test/missingNumber.c
@@ -0,0 +1,14 @@
+int missingNumber(int* nums, int numsSize){
+    int ret=0;
+    for(int i=0;i<numsSize;i++)
+    {
+        ret^=nums[i];
+           
+    }
+
+   for(int i=0;i<=numsSize;i++)
+    {
+        ret^=i;
+    } 
+    return ret;
+}
