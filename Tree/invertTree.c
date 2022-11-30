65f104309da94ecd11f4306a2427b05b7fda3b9e
leiyaling <3120275158@qq.com>
2022-11-30 12:28:17 +0800

    翻转二叉树

---
+++ Tree/invertTree.c
@@ -0,0 +1,14 @@
+//翻转二叉树
+
+struct TreeNode* invertTree(struct TreeNode* root){
+    if(root == NULL)
+        return NULL;
+    
+    struct TreeNode* left = invertTree(root->left);
+    struct TreeNode* right = invertTree(root->right);
+    root->left = right;
+    root->right = left;
+    return root;
+    
+    
+}
