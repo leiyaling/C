b003c86ed960c137ae98bc2c1b1aebd6ecb2d08a
leiyaling <3120275158@qq.com>
2022-12-03 19:22:37 +0800

    希尔排序

---
+++ test/Sort/ShellSort.c
@@ -0,0 +1,27 @@
+void ShellSort(int* a, int n)
+{
+	int gap = n;
+	while (gap > 1)
+	{
+		gap = gap / 3 + 1;
+		for (int i = 0; i < n - gap; i++)
+		{
+			int end = i;
+			int x = a[end + gap];
+			while (end >= 0)
+			{
+				if (x < a[end])
+				{
+					a[end + gap] = a[end];
+					end -= gap;
+				}
+				else
+				{
+					break;
+				}
+			}
+			a[end + gap] = x;
+		}
+	}
+	
+}
